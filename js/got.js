f312d35ba0420072c4f918e77787336f8b458fc5
Zoli Bajan <ivanyimozes@gmail.com>
2018-07-23 13:12:07 +0200

    név szerinti rendezés

--- js/got.js
+++ js/got.js
@@ -1,31 +1,37 @@
 function getData(url, callbackFunc) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            callbackFunc(this);
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function doNotUseUnnamedFunctions() {
+    if (this.readyState === 4 && this.status === 200) {
+      callbackFunc(this);
+    }
+  };
+  xhttp.open('GET', url, true);
+  xhttp.send();
 }
 
 function successAjax(xhttp) {
-    // itt a json content, benne a data változóban
-    var userDatas = JSON.parse(xhttp.responseText);
-    console.log(userDatas);
-    /*
-      Pár sorral lejebb majd ezt olvashatod:
-      IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ!
+  // itt a json content, benne a data változóban
+  var userDatas = JSON.parse(xhttp.responseText);
+  console.log(userDatas);
 
-      Na azokat a függvényeket ITT HÍVD MEG! 
-
-      A userDatas NEM GLOBÁLIS változó, ne is tegyétek ki globálisra. Azaz TILOS!
-      Ha valemelyik függvényeteknek kell, akkor paraméterként adjátok át.
-    */
+  // függvények helye
+  sortByName(userDatas);
 }
 
-// Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
-getData('/json/aJsonFileodNeve.json', successAjax);
+getData('/json/characters.json', successAjax);
 
-// Live servert használd mindig!!!!!
-/* IDE ÍRD A FÜGGVÉNYEKET!!!!!! NE EBBE AZ EGY SORBA HANEM INNEN LEFELÉ! */
+
+// A karakterek megjelenítése név szerint rendezve történjen!
+function sortByName(input) {
+  var i = input.length - 1;
+  while (i > 0) {
+    var swap = 0;
+    for (var j = 0; j < i; j++) {
+      if (input[j].name > input[j + 1].name) {
+        [input[j], input[j + 1]] = [input[j + 1], input[j]];
+        swap = j;
+      }
+    }
+    i = swap;
+  }
+}
